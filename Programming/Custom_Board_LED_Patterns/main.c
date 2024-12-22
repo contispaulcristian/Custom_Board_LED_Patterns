6d4c0f52034314a5856bba1258560795118a209d
Paul Contis <contispaulcristian@gmail.com>
2024-12-22 19:12:56 +0200

    empty project

---
+++ Programming/Custom_Board_LED_Patterns/main.c
@@ -0,0 +1,18 @@
+/*
+ * Custom_Board_LED_Patterns.c
+ *
+ * Created: 12/22/2024 7:07:11 PM
+ * Author : paul.contis
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
